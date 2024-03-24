1141d69342dd357b6d306c2967a2430da41889e3
Jeonni <jeonniu@naver.com>
2024-03-24 20:07:01 +0900

    Feat: 회원가입 페이지 UI 생성

---
+++ client/components/InputGroup.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import cls from "classnames";
+
+interface InutGrouppProps {
+  // classNam? 의 의미: className이 들어올 수도 안들어올 수도 있을 경우
+  className?: string;
+  type?: string;
+  placeholder?: string;
+  value: string;
+  error: string | undefined;
+  setValue: (str: string) => void;
+}
+
+const InputGroup: React.FC<InutGrouppProps> = ({
+  className = "mb-2",
+  type = "text",
+  placeholder = "",
+  value,
+  error,
+  setValue,
+}) => {
+  return (
+    <div className="{className}">
+      <input
+        type={type}
+        style={{ minWidth: 300 }}
+        className={cls(
+          `w-full p-3 transition duration-200 boder boder-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`,
+          { "border-red-500": error }
+        )}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <small className="font-medium text-red-500">{error}</small>
+    </div>
+  );
+};
+
+export default InputGroup;
